ef61fdf67083d349a3aabe670b3a1327a9b20263
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2026-08-03 20:52:08 +0700

    Update engine.js

--- js/core/engine.js
+++ js/core/engine.js
@@ -1,7 +1,9 @@
 window.SpiralGalaxy = window.SpiralGalaxy || {};
 
 window.SpiralGalaxy.Engine = class Engine {
-  constructor({ container, camera: cameraCfg }) {
+  constructor({ container, camera: cameraCfg, post }) {
+    this._post = post || {};
+
     this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: false });
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio || 1, 2));
     this.renderer.setClearColor(0x000000, 1);
@@ -15,9 +17,33 @@ window.SpiralGalaxy.Engine = class Engine {
       cameraCfg.far
     );
 
+    this._setupComposer();
+
     this._onResize = () => this.resize();
     window.addEventListener('resize', this._onResize);
     this.resize();
+  }
+
+  _setupComposer() {
+    this._composer = null;
+    this._bloomPass = null;
+
+    const bloom = this._post.bloom;
+    const hasPostLibs =
+      window.THREE.EffectComposer && window.THREE.RenderPass && window.THREE.UnrealBloomPass;
+
+    if (bloom && bloom.enabled && hasPostLibs) {
+      this._composer = new THREE.EffectComposer(this.renderer);
+      this._composer.addPass(new THREE.RenderPass(this.scene, this.camera));
+
+      this._bloomPass = new THREE.UnrealBloomPass(
+        new THREE.Vector2(window.innerWidth, window.innerHeight),
+        bloom.strength,
+        bloom.radius,
+        bloom.threshold
+      );
+      this._composer.addPass(this._bloomPass);
+    }
   }
 
   get pixelRatio() {
@@ -30,9 +56,17 @@ window.SpiralGalaxy.Engine = class Engine {
     this.camera.aspect = w / h;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(w, h);
+
+    if (this._composer) {
+      this._composer.setSize(w, h);
+    }
   }
 
   render() {
-    this.renderer.render(this.scene, this.camera);
+    if (this._composer) {
+      this._composer.render();
+    } else {
+      this.renderer.render(this.scene, this.camera);
+    }
   }
 };
